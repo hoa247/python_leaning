d4d55f8dbd1b5cfc8332c05fba980a685a44f046
unknown <hoatd@vnext.com.vn>
2021-10-31 06:04:34 +0700

    back folder

--- day2/FileHelper.py
+++ day2/FileHelper.py
@@ -5,7 +5,7 @@ from tkinter import messagebox, filedialog
 class FileHelper():
     def __init__(self):
         self.root = Tk()
-        self.folder = ''
+        self.folder = 'root'
         #inputFolderFrame
         self.inputFolderFrame = LabelFrame(self.root, text="Input folder", height=10)
         self.inputFolderFrame.grid(row=0, column=0, sticky=N+S+W+E, padx=5, pady=5)
@@ -30,6 +30,13 @@ class FileHelper():
         self.folderCheckboxValue = IntVar(None, 1)
         Checkbutton(self.optionsFrame, text="Folder", command=self.handleFilterFileOrFolder,
                     variable=self.folderCheckboxValue).grid(row=1, sticky=W)
+        
+        self.filterNameLabel = Label(self.optionsFrame, text="name")
+        self.filterNameLabel.grid(row=2, column=0,sticky=W+E)
+        self.filterNameEntry = Entry(self.optionsFrame)
+        self.filterNameEntry.grid(row=2, column=1, padx=5, sticky=W+E)
+        self.searchBtn = Button(self.optionsFrame, text="search", command=self.loadScroll, width=10)
+        self.searchBtn.grid(row=3, column=1, padx=5, pady=5, sticky=W)
 
 
         #scroll Folder and file
@@ -45,7 +52,7 @@ class FileHelper():
         self.childFilesScrollbarY = Scrollbar(self.childFilesInFolderFrame)
         self.childFilesScrollbarX.grid(row=1, column=0, sticky=N+S+E+W)
         self.childFilesScrollbarY.grid(row=0, column=1, sticky=N+S+E+W)
-        self.childFilesListBox = Listbox(self.childFilesInFolderFrame)
+        self.childFilesListBox = Listbox(self.childFilesInFolderFrame, width=30)
         self.childFilesListBox.grid(row=0, column=0, sticky=N+S+E+W)
         
         self.childs = []
@@ -66,13 +73,13 @@ class FileHelper():
         self.enterFrame = LabelFrame(self.inputFolderFrame, borderwidth=0)
         self.inputFolderEnterEntry = Entry(self.enterFrame)
         self.inputFolderEnterEntry.grid(row=1, column=0, columnspan=2, padx=5, sticky=W+E)
-        self.inputFolderEnterBtn = Button( self.enterFrame, text="enter", command=self.enterFolder)
+        self.inputFolderEnterBtn = Button(self.enterFrame, text="enter", command=self.enterFolder)
         self.inputFolderEnterBtn.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky=W+E)
         
 
     def handleFilterFileOrFolder(self):
         if self.folder:
-            self.loadScroll(1)
+            self.loadScroll()
     
     def inputFolderTypeHandel(self):
         inputFolderType = self.inputFolderType.get()
@@ -85,10 +92,11 @@ class FileHelper():
             self.inputFolderEnterEntry.delete(0, END)
 
     def chooseFolder(self):
+        self.filterNameEntry.delete(0, END)
         self.folder = filedialog.askdirectory()
         if (not self.folder):
             return print('not choose folder')
-        self.loadScroll(1)
+        self.loadScroll()
     
     def enterFolder(self):
         self.folder = self.inputFolderEnterEntry.get()
@@ -96,20 +104,26 @@ class FileHelper():
             return messagebox.showerror("Warning", "Please enter folder path!")
         if not os.path.isdir(self.folder):
             return messagebox.showerror("Warning", "Folder does not exist!")
-        self.loadScroll(1)
+        self.loadScroll()
 
-    def loadScroll(self, isChoosed = 0):
+    def loadScroll(self):
         self.childFoldersListBox.delete(0, END)
         self.childFilesListBox.delete(0, END)
-        if isChoosed:
+        if self.folder == 'root':
+            dl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            self.childs = ['%s:/' % d for d in dl if os.path.exists('%s:' % d)]
+        else:
             self.childs = os.listdir(self.folder)
             self.childFoldersListBox.insert(END, '../')
+                
         self.filteredChildFolders = []
         self.filteredChildFiles = []
         self.filter()
         
         for childFolder in self.filteredChildFolders:
             self.childFoldersListBox.insert(END, childFolder)
+            
+        self.childFoldersListBox.bind('<Double-Button>', self.doubleClickFolder)
         
         for childFile in self.filteredChildFiles:
             self.childFilesListBox.insert(END, childFile)
@@ -122,15 +136,31 @@ class FileHelper():
         self.childFilesScrollbarX.config(command=self.childFilesListBox.xview)
         self.childFilesScrollbarY.config(command=self.childFilesListBox.yview)
     
+    def doubleClickFolder(self, event):
+        selection = event.widget.curselection()
+        if selection:
+            index = selection[0]
+            clickFolder = event.widget.get(index)
+            if clickFolder == '../' :
+                if (len(self.folder) == 3):
+                    self.folder = 'root'
+                else:
+                    self.folder = os.path.dirname(self.folder)
+            else:
+                self.folder = os.path.join(self.folder, clickFolder)
+        self.loadScroll()
     def filter(self):
         isFile = self.fileCheckboxValue.get()
         isFolder = self.folderCheckboxValue.get()
         for child in self.childs:
+            filterName = self.filterNameEntry.get()
+            if filterName.lower() not in child.lower():
+                continue
             path = self.folder + '/' + child
             if isFile and os.path.isfile(path):
                 self.filteredChildFiles.append(child)
                 continue
-            if isFolder and os.path.isdir(path):
+            if (isFolder and os.path.isdir(path)) or self.folder == 'root':
                 self.filteredChildFolders.append(child)
 
 
